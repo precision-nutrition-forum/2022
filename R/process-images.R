ebd71456fff0686753a5dc5f5405a9a534863cd1
Luke W. Johnston <lwjohnst@gmail.com>
2022-09-11 13:17:16 +0200

    some comments to code

--- R/process-images.R
+++ R/process-images.R
@@ -4,9 +4,15 @@ library(here)
 library(fs)
 library(magick)
 
+# Some images given are in jpg, so want to convert them to png to be consistent.
+# I might also try to process them so they are all consistently the same size, but
+# not size if that would work well.
+
+# The actual pngs can be moved over though.
 dir_ls(here("images/raw"), glob = "*.png") %>%
     file_copy(here("images"))
 
+# Convert jpg to png
 old_jpg_images <- dir_ls(here("images/raw"), glob = "*.jpg")
 new_png_images <- here("images", path_ext_set(path_file(jpg_images), "png"))
 
